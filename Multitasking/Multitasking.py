2d03ab9f40b39ec8910dcc4f7a16a876cdb9a3db
bendordu <63401911+bendordu@users.noreply.github.com>
2021-01-06 23:02:32 +0300

    new

--- Multitasking/Multitasking.py
+++ Multitasking/Multitasking.py
@@ -13,13 +13,12 @@ class Multitasking(QWidget):
 
         self.setWindowTitle('Multitasking')
         self.setStyleSheet("background-color: black;")
-        self.center()
 
         self.count = 0 #счетчик раундов 
 
-        self.correct = 1 #ответы
-        self.wrong = 2
-        self.slow = 3
+        # self.correct = 1 #ответы
+        # self.wrong = 2
+        # self.slow = 3
 
         # self.data = [] #результаты
         # self.load_data() #подгружаем csv табличку с готовыми вариантами тестов 
@@ -28,54 +27,49 @@ class Multitasking(QWidget):
         self.width = QApplication.desktop().width()
         self.height = QApplication.desktop().height()
 
-        # self.right = QPixmap("doc/pictures/right.png") #картиночки
-        # self.bad = QPixmap("doc/pictures/wrong.png")
-        # self.fix = QPixmap("doc/pictures/fix.png")
-        # self.box = QPixmap("doc/pictures/box.png")
-        # self.mask = QPixmap("doc/pictures/mask.png")
-        # self.t1 = QPixmap("doc/pictures/L1.png")
-        # self.n1 = QPixmap("doc/pictures/n1.png")
-        # self.n2 = QPixmap("doc/pictures/n2.png")
-        # self.n3 = QPixmap("doc/pictures/n3.png")
+        self.frame = QPixmap("doc/pictures/frame.png") #картиночки
+        self.s1 = QPixmap("doc/pictures/shape1fill1.png")
+        self.s2 = QPixmap("doc/pictures/shape1fill2.png")
+        self.s3 = QPixmap("doc/pictures/shape2fill1.png")
+        self.s4 = QPixmap("doc/pictures/shape2fill2.png")
+        self.countdown = QPixmap('doc/pictures/countdown.png')
+        self.countdown1 = QPixmap('doc/pictures/countdown1.png')
+        self.countdown2 = QPixmap('doc/pictures/countdown2.png')
+        self.countdown3 = QPixmap('doc/pictures/countdown3.png')
 
-        self.dont_touch = False #чтобы во время показа target нельзя было нажать
-        self.dont_press = True #чтобы не было одновременного нажатия B/N
-        self.dont_next = False #чтобы не переключалось по нжатию на пробел, если не ответа B/N
+        self.dont_press_space = False
 
-        # self.instruction = True #для инструкции, чтобы переключались только слайды
-        # self.instr = 0
-        # self.instr_1()
-        self.test()
+        self.instruction = True #для инструкции, чтобы переключались только слайды
+        self.instr = 1
+        self.instr_1()
         
 
-    # def instr_1(self):
-
-    #     hbox = QHBoxLayout(self)
-    #     title = QPixmap("doc/pictures/title.png")
-    #     self.title = QLabel(self)
-    #     self.title.setPixmap(title)
-    #     hbox.addWidget(self.title)
-    #     self.setLayout(hbox)
-    #     self.title.setAlignment(QtCore.Qt.AlignCenter)
-    #     self.showFullScreen()
-           
-
-    # def instr_2(self):
-    #     instr = QPixmap("doc/pictures/ins_5.png")
-    #     self.title.setPixmap(instr)
+    def instr_1(self):
+        hbox = QHBoxLayout(self)
+        title = QPixmap("doc/pictures/info1.png")
+        self.title = QLabel(self)
+        self.title.setPixmap(title)
+        hbox.addWidget(self.title)
+        self.setLayout(hbox)
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.showFullScreen()
 
 
     def test(self):
-        # self.title.clear()
-        hbox = QHBoxLayout(self)
-        frame = QPixmap("doc/pictures/frame.png")
-        self.frame = QLabel(self)
-        self.frame.setPixmap(frame)
-        self.frame.setGeometry(0, 0, 5, 5)
-        hbox.addWidget(self.frame)
-        self.setLayout(hbox)
-        self.frame.setAlignment(QtCore.Qt.AlignCenter)
-        self.showFullScreen()
+        self.title.setPixmap(self.frame)
+        self.training_1()
+        
+
+    def training_1(self):
+        stimul = self.random_stimul_10()[self.count]
+        picture = f'self.s{stimul}'
+        self.resp = stimul
+        print(stimul)
+        self.stimul_lbl = QLabel(self)
+        self.stimul_lbl.setPixmap(eval(picture))
+        self.stimul_lbl.setGeometry(self.width/2-125, self.height/2-290, 250, 250)
+        self.stimul_lbl.setAlignment(QtCore.Qt.AlignCenter)
+        self.stimul_lbl.show()
 
         # self.instruction = False
                 
@@ -180,13 +174,6 @@ class Multitasking(QWidget):
     #     self.stop = True
     #     self.timer.timeout.connect(self.slow_4sec)
 
-   
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
 
     # def slow_4sec(self):
     #     if self.stop == False:
@@ -199,63 +186,126 @@ class Multitasking(QWidget):
     #         self.data += [self.data_row]
     #         self.stop = False
     #         self.dont_next = False
+
+    # def random_stimul(self):
+    #     list = [1, 2, 3, 4]
+    #     l = []
+    #     for i in range(13):
+    #         shuffle(list)
+    #         l += list
+    #     n = 0 
+    #     p = 0
+    #     while n < len(l)-p-1:
+    #         n += 1
+    #         if l[n] == l[n+1]:
+    #             l.remove(l[n])
+    #             p += 1
+    #     while len(l) != 40:
+    #         l.pop()
+    #     return l
+
+    def random_stimul_10(self):
+        list = [1, 2, 3, 4]
+        l = []
+        for i in range(3):
+            shuffle(list)
+            l += list
+        n = 0 
+        p = 0
+        while n < len(l)-p-2:
+            n += 1
+            if l[n] == l[n+1]:
+                l.remove(l[n])
+                p += 1
+        while len(l) != 10:
+            l.pop()
+        return l
+
+
+    def concentrate(self):
+        self.title.setPixmap(self.countdown)
+        QtTest.QTest.qWait(1000)
+        self.title.setPixmap(self.countdown1)
+        QtTest.QTest.qWait(1000)
+        self.title.setPixmap(self.countdown2)
+        QtTest.QTest.qWait(1000)
+        self.title.setPixmap(self.countdown3)
+        self.test()
     
 
     def keyPressEvent(self, e):
         
         if e.key() == QtCore.Qt.Key_Space:
             if self.instruction:
-                self.instr += 1
-                if self.instr == 1:
-                    self.instr_2()
-                if self.instr == 2:
-                    self.test()
+                if self.instr <= 10:
+                    self.instr += 1
+                    instr = QPixmap('doc/pictures/info' + str(self.instr) + '.png')
+                    self.title.setPixmap(instr)
+                else:
+                    self.concentrate()
+                    self.instruction = False
+                    self.dont_press_space = True
             else:
-                if self.dont_touch == False and self.dont_next == False:
-                    if self.count < 105:
-                        self.count += 1 
-                        self.answer.clear()
-                        self.dont_touch = True
-                        self.dont_next = True
-                        self.processing()
-                    else:
-                        self.write_csv()
-                        self.close()
+                pass
+                # if self.dont_touch == False and self.dont_next == False:
+                #     if self.count < 105:
+                #         self.count += 1 
+                #         self.answer.clear()
+                #         self.dont_touch = True
+                #         self.dont_next = True
+                #         self.processing()
+                #     else:
+                #         self.write_csv()
+                #         self.close()
 
         if e.key() == QtCore.Qt.Key_B:
-            if self.dont_touch == False and self.dont_press == False:
-                self.stop = False
-                self.slow_4sec()
-                rt = round((time() - self.step_1)*1000, 1)
-                self.data_row += [str(rt)]
-                if self.response == 1:
-                    self.answer.setPixmap(self.right)
-                    self.data_row += [str(self.correct)]
+            if self.count < 9:
+                self.count += 1
+                if self.resp == 1 or self.resp == 2:
+                    self.training_1()
                 else:
-                    self.answer.setPixmap(self.bad)
-                    self.data_row += [str(self.wrong)]
-                self.answer.show()
-                self.data += [self.data_row]
-                self.dont_press = True
-                self.dont_next = False
+                    self.stimul_lbl.clear()
+                    self.concentrate()
+            # if self.dont_touch == False and self.dont_press == False:
+            #     self.stop = False
+            #     self.slow_4sec()
+            #     rt = round((time() - self.step_1)*1000, 1)
+            #     self.data_row += [str(rt)]
+            #     if self.response == 1:
+            #         self.answer.setPixmap(self.right)
+            #         self.data_row += [str(self.correct)]
+            #     else:
+            #         self.answer.setPixmap(self.bad)
+            #         self.data_row += [str(self.wrong)]
+            #     self.answer.show()
+            #     self.data += [self.data_row]
+            #     self.dont_press = True
+            #     self.dont_next = False
             
 
         if e.key() == QtCore.Qt.Key_N:
-            if self.dont_touch == False and self.dont_press == False:
-                self.stop = False
-                self.slow_4sec()
-                rt = round((time() - self.step_1)*1000, 1)
-                self.data_row += [str(rt)]
-                if self.response == 1:
-                    self.answer.setPixmap(self.bad)
-                    self.data_row += [str(self.wrong)]
+            if self.count < 9:
+                self.count += 1
+                if self.resp == 3 or self.resp == 4:
+                    self.training_1()
                 else:
-                    self.answer.setPixmap(self.right)
-                    self.data_row += [str(self.correct)]
-                self.answer.show()
-                self.data += [self.data_row]
-                self.dont_press = True
-                self.dont_next = False
+                    self.stimul_lbl.clear()
+                    self.concentrate()
+            # if self.dont_touch == False and self.dont_press == False:
+            #     self.stop = False
+            #     self.slow_4sec()
+            #     rt = round((time() - self.step_1)*1000, 1)
+            #     self.data_row += [str(rt)]
+            #     if self.response == 1:
+            #         self.answer.setPixmap(self.bad)
+            #         self.data_row += [str(self.wrong)]
+            #     else:
+            #         self.answer.setPixmap(self.right)
+            #         self.data_row += [str(self.correct)]
+            #     self.answer.show()
+            #     self.data += [self.data_row]
+            #     self.dont_press = True
+            #     self.dont_next = False
             
         if e.key() == QtCore.Qt.Key_Escape:
             # self.write_csv()
